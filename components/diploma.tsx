8ca88f29b4e5a16b5cb14e09cf1bff3f527a82ea
Ivan Rios <ivanerioshuete@gmail.com>
2022-08-17 16:10:08 +0200

    Maquetado a falta del footer

--- components/diploma.tsx
+++ components/diploma.tsx
@@ -1,11 +1,11 @@
 import styles from './diploma.module.css'
 
-const Diploma = (/*{name}: {name: any}, {course}: {course: any}, {date}: {date: any}*/) => {
+const Diploma = ({name, course, date}) => {
   return (
       <div className={styles.diploma}>
-        <div className={styles.name}>Álvaro García</div>
-        <div className={styles.course}>Java Avanzado</div>
-        <div className={styles.date}>12-04-2022</div>
+        <div className={styles.name}>{name}</div>
+        <div className={styles.course}>{course}</div>
+        <div className={styles.date}>{date}</div>
       </div>
   )
 }
---
+++ components/main.tsx
@@ -0,0 +1,42 @@
+import styles from './main.module.css'
+import Diploma from '../components/diploma'
+import badge from '../utils/images/badge.svg'
+import link from '../utils/images/link.svg'
+import download from '../utils/images/download.svg'
+import smiley from '../utils/images/smiley.svg'
+import Image from 'next/image'
+
+const data = {
+  name: 'Álvaro García',
+  course: 'Java Avanzado',
+  date: '12-04-2022'
+}
+
+const Main = () => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.title}>
+        <div className={styles.icon}>
+          <span><Image src={badge} width={38} height={38} /></span>
+        </div>
+        <div className={styles.text}>
+          <span>CERTIFICAIÓN OBTENIDA</span>
+          <span>{data.course}</span>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <Diploma name={data.name} course={data.course} date={data.date}/>
+      </div>
+      <div className={styles.diplomaMenu}>
+        <div className={styles.heading}><Image src={smiley} width={48} height={48} /><span>Completado por <strong>{data.name}</strong></span></div>
+        <div className={styles.text}>OpenBootcamp certifica que <strong>{data.name}</strong> ha finalizado y obtenido con éxito la certificación de <u>{data.course}</u>.</div>
+        <div className={styles.buttons}>
+          <button><Image src={link} width={16} height={17} />Compartir</button>
+          <button><Image src={download} width={17} height={14} />Descargar en PDF</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
